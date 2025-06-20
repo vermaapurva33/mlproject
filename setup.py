61feaf2bf5158be096829e59d6949f2a18f16fd6
vermaapurva33 <vermaapurva33@gmail.com>
2025-06-21 00:26:05 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages,setup 
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path=str)-> List [str]:
+    '''
+    this function will return the list of rquirements
+    '''
+    requirements = []
+    with open (file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+
+
+    name="mlproject",
+    version="0.0.1",
+    author="apurva",
+    author_email="vermaapurva33@gmail.com",
+    packages = find_packages(),
+    # install_requires = ['pandas','numpy','flask','scikit-learn','seaborn','matplotlib','tensorflow','keras','pydantic'],
+    install_requires = get_requirements('requirements.txt')
+
+)
